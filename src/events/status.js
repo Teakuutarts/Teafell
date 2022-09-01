13becbe59a9a0cf0b30299863d16a1f130f06d5b
Ace <77860646+AceSardonyx@users.noreply.github.com>
2022-09-01 11:36:51 -0400

    status moved to events

--- src/events/status.js
+++ src/events/status.js
@@ -3,6 +3,7 @@ const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES,],
 	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
+const { token } = require("../config.json");
 const { username } = require("../config.json");
 const { status } = require("../config.json");
 client.on('ready', () => {
