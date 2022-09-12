57cdc7f7281be8ebd4fb1526907930bf306f969b
Ace <77860646+AceSardonyx@users.noreply.github.com>
2022-09-12 13:50:44 -0400

    new cmdlet load msg

--- src/commands/initial_handler.js
+++ src/commands/initial_handler.js
@@ -12,7 +12,7 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 (async () => {
 	try {
-		console.log('tt! enabled :D');
+		console.log('Teakuu Commands Enabled');
 		await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
